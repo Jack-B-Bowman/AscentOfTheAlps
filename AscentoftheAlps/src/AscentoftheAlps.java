17d2d0b71965f65b3a90a828858a1ea8f765100c
1AidanJ <51326009+1AidanJ@users.noreply.github.com>
2019-06-05 09:23:33 -0400

    git bash is really trash

--- AscentoftheAlps/src/AscentoftheAlps.java
+++ AscentoftheAlps/src/AscentoftheAlps.java
@@ -22,7 +22,7 @@ public class AscentoftheAlps {
     boolean haschef = false;
     int partynum = 0;
     double eatfood;
-
+    
     /**
      *
      * @param args the command line arguments
@@ -30,18 +30,18 @@ public class AscentoftheAlps {
     public static void main(String[] args) {
     }
 
-    public void move() {
+    public void day() {
 
 
         if (stop = true) {
+            
         } else {
             while (disttraveled != distance) {
-                disttraveled++;
-                //keep going until disstraveled has counted up to = distance
-                System.out.println(disttraveled);
-                if (disttraveled % 24 == 0) {
-                    //every 10 nth distance do a food operation
+                
+               
 
+                System.out.println(disttraveled);
+                
                     if (haschef = true) {
                         eatfood = partynum - 1;
                         food = food - eatfood;
@@ -54,17 +54,29 @@ public class AscentoftheAlps {
                         System.out.println("You have " + food + "units of food left");
                         // calculations differ if haschef = false (use default food)
                     }
-                }
+                
 
-
+                 
 
                 if (eventtrigger = true) {
                     break;
+                    
                     //travel forever until an event happens, then break out of the loop
                 }
+                if (stop = true) {
+                    break;
+                }
+            
+            
             }
 
         }
 
     }
-}
+    
+   
+    
+    
+    }
+    
+
