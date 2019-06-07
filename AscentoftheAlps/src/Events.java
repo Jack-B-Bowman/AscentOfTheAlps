5c5d7352fa42d697ebfc9e23e0b7ab635a5b3e5c
TaylorConn <tayloraconn@gmail.com>
2019-06-07 09:24:53 -0500

    lol

--- AscentoftheAlps/src/Events.java
+++ AscentoftheAlps/src/Events.java
@@ -1,8 +1,11 @@
+import java.util.Random;
 /**
  *
  * @author Taylor Conn
  */
 public class Events
 {
+    static Random actOfGod = new Random();
+    static int chance = actOfGod.nextInt(50);
     
 }
