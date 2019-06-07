c375151d23968e9c2ea2fec1434cf8dab03e8133
1bowmanjac <1bowmanjac@gmail.com>
2019-06-07 09:35:00 -0400

    yougay

--- AscentoftheAlps/src/AnimationWindow.java
+++ AscentoftheAlps/src/AnimationWindow.java
@@ -1,17 +1,30 @@
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class AnimationWindow extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
-	public AnimationWindow() {
-            JLabel Background = new JLabel("Background");
-            JLabel Event = new JLabel("Event");
-            JLabel Elephant = new JLabel("Elephants");
-            
-            Background.setBounds(0,0,785,215);
-            
-	}
+    /**
+     * Create the panel.
+     */
+    public AnimationWindow() throws IOException {
+        JLabel background = new JLabel("Background");
+        JLabel event = new JLabel("Event");
+        JLabel elephant = new JLabel("Elephants");
+        BufferedImage pic = ImageIO.read(this.getClass().getResource("/Elephant.png"));
 
+
+        background.setBounds(0, 0, 785, 215);
+        add(background);
+        
+        
+        elephant.setBounds(688,155,97,60);
+        elephant.setIcon(new ImageIcon(pic));
+        add(elephant);
+
+
+
+    }
 }
