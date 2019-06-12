52e759854e8e35b628b49b5df028068bf73942ba
1AidanJ <51326009+1AidanJ@users.noreply.github.com>
2019-06-12 09:30:46 -0400

    Microwaves be like MMMMMMMHMMMMHMHMMHMMMMMMMM

--- AscentoftheAlps/src/AscentoftheAlps.java
+++ AscentoftheAlps/src/AscentoftheAlps.java
@@ -7,7 +7,9 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.*;
-
+import java.applet.*;
+import sun.audio.*;
+import java.io.*;
 /**
  *
  * @author 1mccubbinaid
@@ -28,6 +30,23 @@ public class AscentoftheAlps {
      */
     public static void main(String[] args) {
         GUI.main(null);
+
+        //do not touch this is experimental code from me
+
+        // AudioClip chiptune = getAudioClip(getCodeBase(), soundFile);
+        InputStream in = new FileInputStream();
+// Create an AudioStream object from the input stream.
+AudioStream as = new AudioStream(in);
+// Use the static class member "player" from class AudioPlayer to play
+// clip.
+AudioPlayer.player.start(as);
+// Similarly, to stop the audio.
+AudioPlayer.player.stop(as);
+
+
+
+
+
     }
 
     public static void day() {
@@ -36,7 +55,7 @@ public class AscentoftheAlps {
                     Inventory.Distance -= Var.distancePerDay;
                     Events.runEvents();
 //when day is triggered, run foodconsumption, distance and roll for events
-       
+   
 
 
     }
